8807425eb2ffc081068b0b2f7c7064d578f5ae05
suhyun5252 <o52o.suhyun@gmail.com>
2024-12-06 15:22:58 +0900

    [프로젝트 생성]

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import IndexPage from "./pages/IndexPage";
+
+function App() {
+  return <IndexPage />;
+}
+export default App;
---
+++ src/pages/IndexPage.jsx
@@ -0,0 +1,14 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+function IndexPage() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+export default IndexPage;
