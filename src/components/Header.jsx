f24dfec5036e4cb5cd76a6905f8a28b6f2aab4c9
suhyun5252 <o52o.suhyun@gmail.com>
2024-12-06 18:00:41 +0900

    [이수현] Header 작업완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,41 @@
+import styled from "@emotion/styled";
+import { GnbUl, HeaderDiv, MainMenuDiv, SubmenuUl } from "./Estyle";
+const headerMenu = ["MENU-1", "MENU-2", "MENU-3", "MENU-4"];
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <HeaderDiv>
+      <div className="left">
+        <a href="#" className="logo">
+          logo
+        </a>
+      </div>
+      <div className="right">
+        <GnbUl>
+          {headerMenu.map((item, index) => {
+            return (
+              <MainMenuDiv key={index}>
+                <a href="#">{item}</a>
+                <SubmenuUl>
+                  <li>
+                    <a href="#">submenu-1</a>
+                  </li>
+                  <li>
+                    <a href="#">submenu-2</a>
+                  </li>
+                  <li>
+                    <a href="#">submenu-3</a>
+                  </li>
+                  <li>
+                    <a href="#">submenu-4</a>
+                  </li>
+                </SubmenuUl>
+              </MainMenuDiv>
+            );
+          })}
+        </GnbUl>
+      </div>
+    </HeaderDiv>
+  );
 };
 export default Header;
--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,14 +1,15 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Main from "./components/Main";
+import { Layout } from "../components/Estyle";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Main from "../components/Main";
 
 function IndexPage() {
   return (
-    <div>
+    <Layout>
       <Header />
       <Main />
       <Footer />
-    </div>
+    </Layout>
   );
 }
 export default IndexPage;
